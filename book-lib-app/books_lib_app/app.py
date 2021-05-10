74174718fbb8b75b902e534d86af61a334507ead
EC2 Default User <ec2-user@ip-172-31-93-13.ec2.internal>
2021-05-10 11:25:06 +0000

    first commit

--- book-lib-app/books_lib_app/app.py
+++ book-lib-app/books_lib_app/app.py
@@ -1,22 +1,59 @@
-import json
+import simplejson as json
 import boto3 
 import os 
+from boto3.dynamodb.conditions import Key
+
 
 dynamodb = boto3.resource('dynamodb')
 table_name = os.environ.get('BOOKS_TABLE')
 
 def lambda_handler(event, context):
     
-    print(event)
-    # order = json.loads(event["body"])
-
-    # table = dynamodb.Table(table_name)
-    # response = table.put_item(Item=order)
+    table = dynamodb.Table(table_name)
+    method_type = event["httpMethod"]
     
-    # print(response)
+    # Get list of books from library (All Books and Single Book)
+    if method_type == "GET":
+        
+        # When Book ID is provided
+        if event["queryStringParameters"]:
+            book_isbn = int(event["queryStringParameters"]["isbn_id"])
+            book = table.query(KeyConditionExpression=Key("isbn_id").eq(book_isbn))
+        
+        # When Book ID is NOT provided       
+        else:
+            book = table.scan()
+        
+        return {
+            'statusCode' : 200,
+            'headers' : {},
+            'body' : json.dumps(book['Items'])
+            }
     
-    return {
-        "statusCode" : 201,
-        "headers" : {},
-        "body" : json.dumps({"message": "Fake Massage"})
-    }
+    # Add book(s) to the library
+    if method_type == "POST":
+        
+        book_data = json.loads(event["body"])
+        
+        for each_book in book_data:
+            table.put_item(Item=each_book)
+        
+        return {
+            "statusCode" : 201,
+            "headers" : {},
+            "body" : json.dumps({"message": "Books data updated"})
+        }
+    
+    # Delete a book from the library
+    if method_type == "DELETE":
+        
+        # When Book ID is provided
+        if event["queryStringParameters"]:
+            book_isbn = int(event["queryStringParameters"]["isbn_id"])
+            table.delete_item(Key={"isbn_id": book_isbn})
+        
+        return {
+            "statusCode" : 201,
+            "headers" : {},
+            "body" : json.dumps({"message": f"Book deleted with isbn : {book_isbn}"})
+        }
