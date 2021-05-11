06e26e5683897b9f5b7045b32a66ebabd7df518c
EC2 Default User <ec2-user@ip-172-31-93-13.ec2.internal>
2021-05-11 08:30:32 +0000

    adding the lambda-comprehend function

--- first-app/lambda_handler.py
+++ first-app/lambda_handler.py
@@ -8,24 +8,21 @@ comprehend = boto3.client(service_name="comprehend")
 # create a s3 object 
 s3 = boto3.client("s3")
 
-BUCKET_NAME = os.environ.get('BUCKET_NAME')
-
 def lambda_handler(event, context):
     
     # check if an event is True 
     if event:
         
+        
         # event – AWS Lambda uses this parameter to pass in event data to the handler. 
         # This parameter is usually of the Python dict type. It can also be list, str, int, float, or NoneType type
         text_file_obj = event["Records"][0]
         # assign the uploaded text file name to the 'filename' variable 
-        filename = str(text_file_obj['s3']['object']['key'])
         
-        # print the filename 
-        print("filename: ", filename)
-
-        # create the file object
-        file_obj = s3.get_object(Bucket = BUCKET_NAME, Key = filename)
+        filename = str(text_file_obj['s3']['object']['key'])
+        bucket_name = text_file_obj['s3']['bucket']['name']
+        
+        file_obj = s3.get_object(Bucket = bucket_name, Key = filename)
         
         # access the file_obj's body. Invoke the read() function and convert to a str object. 
         # assign to the variable 'body_str_obj' 
